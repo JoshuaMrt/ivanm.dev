dfa9cd58fe26d9ce7c2ca81261bd5ed3c102b21d
imerete <imerete@blastasia.com>
2024-06-21 20:12:21 +0800

    update branch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,17 +25,45 @@ constructor () {
       y: -20,
       stagger: 0.15
     });
-  }
 
-  ngAfterViewInit(): void {
-    // Additional animations in ngAfterViewInit if necessary
-    gsap.from('.body-page img, .body-page me-image, .body-page div', {
+    gsap.from('.body-page .active, .body-page .name, .body-page .profesion, .body-page .butoons, .body-page .skill-text', {
       delay: 0.5,
       duration: 0.4,
       opacity: 0,
       y: -20,
       stagger: 0.15
     });
+
+    gsap.from('.body-page .project-item, .body-page .text-lets, .body-page .text-lets-about', {
+      delay: 0.5,
+      duration: 0.4,
+      opacity: 0,
+      y: -20,
+      stagger: 0.15
+    });
+
+    gsap.from('.body-page .btn-contact, .body-page a, .body-page .follow, .body-page .credit-name', {
+      delay: 0.5,
+      duration: 0.4,
+      opacity: 0,
+      y: -20,
+      stagger: 0.15
+    });
+
+  }
+  
+
+  ngAfterViewInit(): void {
+    // Additional animations in ngAfterViewInit if necessary
+    gsap.from('.body-page img, .body-page me-image', {
+      delay: 0.5,
+      duration: 0.4,
+      opacity: 0,
+      y: -20,
+      stagger: 0.15
+    });
+
+   
   }
   copyEmail() {
     this.isCopying = true;
